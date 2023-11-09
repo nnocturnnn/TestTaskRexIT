037ee117bf125f29a0d21adb8c408d7af8ce1ff8
Andrii <55098895+nnocturnnn@users.noreply.github.com>
2023-11-09 03:30:51 -0500

    feat: update filters

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 from flask import Flask, render_template, request
-import sqlite3
 import misc
 import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     page = request.args.get('page', 1, type=int)
     per_page = 10
+    filters = None
+
     if request.method == 'POST' and 'csv' in request.files:
         csv_file = request.files['csv']
         if csv_file:
             csv_file.save(os.path.join(app.config['UPLOADED_CSV_DEST'], csv_file.filename))
-    return render_template('index.html', data=misc.get_data(per_page, page), page=page, per_page=per_page)
+
+    if request.method == 'GET':
+        filter_params = ['category', 'gender', 'dob', 'min_age', 'max_age']
+        filters = {param: request.args.get(param) for param in filter_params if request.args.get(param) is not None}
+        
+    data = misc.get_filtered_data(per_page, page, filters)
+    return render_template('index.html', data=data, page=page, per_page=per_page)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- misc.py
+++ misc.py
@@ -1,7 +1,7 @@
 import csv
 import sys
 import sqlite3
-from typing import Iterator, Dict
+from typing import Iterator, Dict, List, Optional
 
 def read_csv_iterator(file_path: str) -> Iterator[Dict[str, str]]:
     with open(file_path, mode='r', newline='') as file:
@@ -12,19 +12,48 @@ def read_csv_iterator(file_path: str) -> Iterator[Dict[str, str]]:
 def create_and_populate_database(data_iterator: Iterator[Dict[str, str]], database_path: str="users.db") -> None:
     conn = sqlite3.connect(database_path)
     cursor = conn.cursor()
-    create_table_query = f'CREATE TABLE IF NOT EXISTS users (category TEXT, firstname TEXT, lastname TEXT, email TEXT, gender TEXT, birthDate TEXT);'
+    create_table_query = f'CREATE TABLE IF NOT EXISTS users (category TEXT, firstname TEXT, lastname TEXT, email TEXT, gender TEXT, dob TEXT);'
     cursor.execute(create_table_query)
     for row in data_iterator:
-        insert_query = f'INSERT INTO users (category, firstname, lastname, email, gender, birthDate) VALUES (:category, :firstname, :lastname, :email, :gender, :birthDate);'
+        insert_query = f'INSERT INTO users (category, firstname, lastname, email, gender, dob) VALUES (:category, :firstname, :lastname, :email, :gender, :birthDate);'
         cursor.execute(insert_query, row)
     conn.commit()
     conn.close()
 
-def get_data(per_page, page):
+def get_filtered_data(per_page: int, page: int, filters: Optional[Dict[str, str]] = None) -> List[Dict[str, str]]:
     conn = sqlite3.connect("users.db")
     conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
-    cursor.execute(f'SELECT * FROM users LIMIT ? OFFSET ?', (per_page, (page - 1) * per_page))
+
+    query = "SELECT * FROM users WHERE 1"
+    params = []
+
+    if filters:
+        print("qqq")
+        if 'category' in filters:
+            query += " AND category = ?"
+            params.append(filters['category'])
+        if 'gender' in filters:
+            query += " AND gender = ?"
+            params.append(filters['gender'])
+        if 'dob' in filters:
+            query += " AND dob = ?"
+            params.append(filters['dob'])
+        if 'min_age' in filters and 'max_age' in filters:
+            query += " AND strftime('%Y', 'now') - strftime('%Y', dob) BETWEEN ? AND ?"
+            params.extend([filters['min_age'], filters['max_age']])
+        elif 'min_age' in filters:
+            query += " AND strftime('%Y', 'now') - strftime('%Y', dob) >= ?"
+            params.append(filters['min_age'])
+        elif 'max_age' in filters:
+            query += " AND strftime('%Y', 'now') - strftime('%Y', dob) <= ?"
+            params.append(filters['max_age'])
+
+    query += " LIMIT ? OFFSET ?"
+    params.extend([per_page, (page - 1) * per_page])
+    
+    cursor.execute(query, params)
+
     data = cursor.fetchall()
 
     cursor.close()
