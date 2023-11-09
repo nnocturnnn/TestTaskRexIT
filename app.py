bed375847cdae071e5931eaefd0e3409cb404501
Andrii <55098895+nnocturnnn@users.noreply.github.com>
2023-11-09 18:04:09 -0500

    fix: codestyle

--- app.py
+++ app.py
@@ -1,33 +1,28 @@
 from flask import Flask, render_template, request
-from flask_uploads import UploadSet, configure_uploads, DATA
 import misc
 import os
 
 app = Flask(__name__)
 
-app.config['UPLOADED_CSV_DEST'] = 'uploads/csv'
-csv_files = UploadSet('csv', DATA)
-configure_uploads(app, (csv_files,))
+app.config['UPLOADED_CSV_DEST'] = 'uploads/'
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     page = request.args.get('page', 1, type=int)
-    per_page = 100
-    filters = None
+    per_page = 10
 
     if request.method == 'POST' and 'csv' in request.files:
         csv_file = request.files['csv']
         if csv_file:
-            csv_file.save(os.path.join(app.config['UPLOADED_CSV_DEST'], csv_file.filename))
+            filepath = os.path.join(app.config['UPLOADED_CSV_DEST'], csv_file.filename)
+            csv_file.save(filepath)
+            misc.create_user_table()
+            misc.populate_user_table(misc.read_csv_iterator(filepath))
 
-    if request.method == 'GET':
-        filter_params = ['category', 'gender', 'dob', 'min_age', 'max_age']
-        filters = {param: value for param, value in request.args.items() if value}
+    filters = {param: value for param, value in request.args.items() if value} if request.method == 'GET' else None
     data = misc.get_filtered_data(per_page, page, filters)
     return render_template('index.html', data=data, page=page, per_page=per_page)
-
-if __name__ == '__main__':
-    app.run(debug=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- misc.py
+++ misc.py
@@ -1,7 +1,7 @@
 import csv
 import sys
 import sqlite3
-from typing import Iterator, Dict, List, Optional
+from typing import Iterator, Dict, List, Optional,Any, Tuple
 
 def read_csv_iterator(file_path: str) -> Iterator[Dict[str, str]]:
     with open(file_path, mode='r', newline='') as file:
@@ -9,19 +9,30 @@ def read_csv_iterator(file_path: str) -> Iterator[Dict[str, str]]:
         for row in reader:
             yield row
 
-def create_and_populate_database(data_iterator: Iterator[Dict[str, str]], database_path: str="users.db") -> None:
-    conn = sqlite3.connect(database_path)
-    cursor = conn.cursor()
-    create_table_query = f'CREATE TABLE IF NOT EXISTS users (category TEXT, firstname TEXT, lastname TEXT, email TEXT, gender TEXT, dob DATETIME);'
-    cursor.execute(create_table_query)
-    for row in data_iterator:
-        insert_query = f'INSERT INTO users (category, firstname, lastname, email, gender, dob) VALUES (:category, :firstname, :lastname, :email, :gender, :birthDate);'
-        cursor.execute(insert_query, row)
-    conn.commit()
-    conn.close()
+
+def create_user_table(database_path: str = "users.db") -> None:
+    try:
+        with sqlite3.connect(database_path) as conn:
+            cursor = conn.cursor()
+            create_table_query = 'CREATE TABLE IF NOT EXISTS users (category TEXT, firstname TEXT, lastname TEXT, email TEXT, gender TEXT, dob DATETIME);'
+            cursor.execute(create_table_query)
+            conn.commit()
+    except sqlite3.Error as e:
+        print("SQLite error:", e)
 
 
-def get_filtered_data(per_page: int, page: int, filters: Optional[Dict[str, str]] = None) -> List[Dict[str, str]]:
+def populate_user_table(data_iterator: Iterator[Dict[str, str]], database_path: str = "users.db") -> None:
+    try:
+        with sqlite3.connect(database_path) as conn:
+            cursor = conn.cursor()
+            for row in data_iterator:
+                insert_query = 'INSERT INTO users (category, firstname, lastname, email, gender, dob) VALUES (:category, :firstname, :lastname, :email, :gender, :birthDate);'
+                cursor.execute(insert_query, row)
+            conn.commit()
+    except sqlite3.Error as e:
+        print("SQLite error:", e)
+
+def build_filter_query(filters: Optional[Dict[str, str]]) -> Tuple[str, List[Any]]:
     query = "SELECT * FROM users WHERE 1"
     params = []
 
@@ -42,20 +53,24 @@ def get_filtered_data(per_page: int, page: int, filters: Optional[Dict[str, str]
         elif 'max_age' in filters:
             query += " AND DATE('now') <= DATE(dob, '+{0} years')".format(filters['max_age'])
 
+    return query, params
 
+def execute_query(query: str, params: List[Any], per_page: int, page: int) -> List[Dict[str, str]]:
     query += " LIMIT ? OFFSET ?"
-    print(query)
     params.extend([per_page, (page - 1) * per_page])
-    print(params)
 
-    with sqlite3.connect("users.db") as conn:
-        conn.row_factory = sqlite3.Row
-        cursor = conn.cursor()
-        cursor.execute(query, params)
-        data = cursor.fetchall()
-    return data
+    try:
+        with sqlite3.connect("users.db") as conn:
+            conn.row_factory = sqlite3.Row
+            cursor = conn.cursor()
+            cursor.execute(query, params)
+            data = cursor.fetchall()
+        return data
+    except sqlite3.Error as e:
+        print("SQLite error:", e)
+        return []
 
+def get_filtered_data(per_page: int, page: int, filters: Optional[Dict[str, str]] = None) -> List[Dict[str, str]]:
+    query, params = build_filter_query(filters)
+    return execute_query(query, params, per_page, page)
 
-if __name__ == '__main__':
-    data_iterator = read_csv_iterator(sys.argv[1])
-    create_and_populate_database(data_iterator)
